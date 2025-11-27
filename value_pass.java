a40b91996779d25ae0cc4495ae6a101499c1dc34
rezwan-raj <rezwanahmedcse@gmail.com>
2025-11-27 19:49:22 +0600

    third code is done

---
+++ value_pass.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class value_pass {
+    public static void main(String[]args){
+        r("raj",10);
+
+    }
+    public static void r(String name,float a){
+
+        System.out.println(" "+name);
+        
+        System.out.println(" "+a);
+
+    }
+}
