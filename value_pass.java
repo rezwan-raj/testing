f02b7173851dc2d7c3ff2ea75bcc08605caf64bc
MD Al Rakib Sorder <46457169+saikat111@users.noreply.github.com>
2025-11-27 20:01:55 +0600

    valuepass bug fix

--- value_pass.java
+++ value_pass.java
@@ -2,14 +2,12 @@ import java.util.Scanner;
 
 public class value_pass {
     public static void main(String[]args){
-        r("raj",10);
+         System.out.println(r("raj",10));
 
     }
-    public static void r(String name,float a){
-
-        System.out.println(" "+name);
-        
-        System.out.println(" "+a);
-
+    public static String r(String name,float a){
+        String sum;
+        sum = name + String.valueOf(a);
+        return sum;
     }
 }
